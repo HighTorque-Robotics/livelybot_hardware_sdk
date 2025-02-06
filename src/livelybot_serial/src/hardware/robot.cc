7e478e02584311b58e7b38d1dc54fc7a402f5f0e
千里客 <2812929818@qq.com>
2025-02-06 18:49:22 +0800

    v3.9 修复使用text_motor_run回零位电机不动问题

--- src/livelybot_serial/src/hardware/robot.cc
+++ src/livelybot_serial/src/hardware/robot.cc
@@ -412,15 +412,6 @@ namespace livelybot_serial
         }
         motor_send_2();
     #endif
-
-        for (int i = 0; i < 3; i++)
-        {
-            set_reset();
-            motor_send_2();
-            ros::Duration(0.001).sleep();
-        }
-
-        ros::Duration(0.1).sleep();
     }
 
 
@@ -482,6 +473,15 @@ namespace livelybot_serial
             // exit(-1);
             ros::Duration(1).sleep();
         }
+
+        for (int i = 0; i < 3; i++)
+        {
+            set_reset();
+            motor_send_2();
+            ros::Duration(0.001).sleep();
+        }
+
+        ros::Duration(0.1).sleep();
     }
 
 
